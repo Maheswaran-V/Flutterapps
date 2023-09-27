2df09d3970df8e49af7ef010ce61d0f2589d3418
Maheswaran V <maheswaran@senstornet.com>
2023-09-27 15:19:53 +0530

    newlyinstalled

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Flutter Application"),
+          title: const Text("Android Application"),
         ),
         body: const SingleChildScrollView(
           child: Column(
